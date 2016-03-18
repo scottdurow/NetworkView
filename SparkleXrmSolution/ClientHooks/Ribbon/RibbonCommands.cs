a3e0634dcff726ed04a1373ffb54657c24be953f
scottdurow <scott_durow@hotmail.com>
2016-03-18 17:24:28 +0000

    Generic Ribbon Command JS

---
+++ SparkleXrmSolution/ClientHooks/Ribbon/RibbonCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xrm;
+
+
+namespace NetworkView.ClientHooks.Ribbon
+{
+    public static class RibbonCommands
+    {
+        public static void OpenNetworkViewFromForm()
+        {
+            OpenNetworkView(Page.Data.Entity.GetId(), Page.Data.Entity.GetEntityName());   
+        }
+        public static void OpenNetworkView(string id, string etn)
+        {
+            string data = string.Format("id={0}&etn={1}", id, etn);
+            data = GlobalFunctions.encodeURIComponent(data);
+            Utility.OpenWebResource("dev1_/html/NetworkView.htm", data, 1024, 768);
+        }
+    }
+}
